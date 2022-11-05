108789d34f3b15ba35c9d08e5ac6e492b39dcdf3
martin <tin.maldonado@gmail.com>
2022-11-05 18:41:38 -0300

    initial commit

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  arrowParens: 'avoid',
+  semi: true,
+  bracketSameLine: true,
+  bracketSpacing: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  endOfLine: 'auto',
+};
---
+++ test/index.test.ts
@@ -0,0 +1,5 @@
+test('example', () => {
+  const sum = 1 + 1;
+  const result = 2;
+  expect(sum).toBe(result);
+});
